4694e2be85d6d8b6f9cb73422570e10cf6ae06af
Archie Macdonald <archiemac07@outlook.com>
2025-12-13 18:05:57 +0000

    Added man style docs and finished display functionality.
    Man style docs are under usage.txt, printed when the user doesn't supply
    any arguments
    
    Finished the Display function, it can take two addtional arguments for
    sorting and filtering as per the docs.

--- TODO.bash
+++ TODO.bash
@@ -26,12 +26,11 @@ print_sorted_csv() {
         return 1
     fi
 
-    # 1. Grab the Header (first line)
-    # 2. Grab the Body (lines 2 to end), Sort it, then append to Header
-    # 3. Format with column (same as before)
+
+
     (
-        head -n 1 "$file"
-        tail -n +2 "$file" | sort -t, -k"${col}"
+        head -n 1 "$file" # The header line
+        tail -n +2 "$file" | sort -t, -k"${col}" # The data itself
     ) | column -s, -t -o " | "
 }
 
@@ -53,11 +52,87 @@ get_column_from_name() {
             break
         fi
     done
-    
+
     echo $col_index
 }
 
+print_filtered_csv() {
+    local file="$1"
+    local col="$2"
+    local filter="$3"
 
-column_number=$(get_column_from_name tasks.csv Due)
-echo "Sorting by column number: $column_number"
-print_sorted_csv tasks.csv $column_number
+    # Check for arguments
+    if [[ -z "$file" || -z "$col" ]]; then
+        echo "Usage: print_filtered_csv <filename> <column_number>"
+        return 1
+    fi
+
+
+
+    (
+        head -n 1 "$file"
+        tail -n +2 "$file" | sort -t, -k"${col}" # The data itself
+    ) | column -s, -t -o " | " | grep -e "$filter" -e "Name"
+}
+
+display() {
+    local col_index="$1"
+    local filter="$2"
+    if [[ -z "$col_index" ]]; then
+        # if it is empty, we make it priority
+        col_index=3 # Default to priority
+    else
+        col_index=$(get_column_from_name tasks.csv "$col_index")
+    fi
+
+    if [[ -z "$filter" ]]; then
+        print_sorted_csv tasks.csv "$col_index"
+    else
+        print_filtered_csv tasks.csv "$col_index" "$filter"
+    fi
+}
+
+# Main goes in here
+
+while getopts ":dar" flag; do
+    case "${flag}" in
+        d)
+            # To allow for Optional Arguments, we need to do a little hack.
+            # First we use OPTIND to 'steal' the next argument.
+            next_arg="${!OPTIND}"
+
+            #Then we check if the next argument exists and isn't flagged.
+            if [[ -n "$next_arg" && "$next_arg" != -* ]]; then
+                #When we get a match, we set OPTARG to it.
+                OPTARG="$next_arg"
+
+                # IMPORTANT: We must manually tell getopts to skip the next argument (since we stole it)
+                # so it doesn't try to process the args in the next pass.
+                OPTIND=$((OPTIND + 1))
+            else
+                # No argument found (or next was a flag like -a)
+                OPTARG=""
+            fi
+
+            read -r -a args <<< "$OPTARG"
+            display "${args[0]}" "${args[1]}"
+            exit 0
+            ;;
+        a)
+            echo "Adding Not implemented"
+            exit 0
+            ;;
+        r)
+            echo "Removing Not implemented"
+            exit 0
+            ;;
+        *)
+            echo "Invalid option: -$OPTARG"
+            exit 1
+            ;;
+    esac
+done
+
+#Force people to READ!
+echo "Error: No arguments provided!"
+cat usage.txt
